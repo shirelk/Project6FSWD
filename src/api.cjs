13120dd8b7cf39822c89ef14239120692d44ac49
Shirel <shkadosh@g.jct.ac.il>
2023-06-20 17:54:18 +0300

    GET, POST, PUT, DELETE for users, todos, posts and comments

--- src/api.cjs
+++ src/api.cjs
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const db = require("./MySQL.cjs"); // Import the database connection module
 
-// Define your API endpoints
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+//------------------------------------USERS-------------------------------------
 app.get("/users", (req, res) => {
   db.query("SELECT * FROM users", (err, results) => {
     if (err) {
@@ -14,6 +17,82 @@ app.get("/users", (req, res) => {
   });
 });
 
+app.get("/users/:id", (req, res) => {
+  const userId = req.params.id;
+  db.query("SELECT * FROM users WHERE id = ?", [userId], (err, results) => {
+    if (err) {
+      console.error("Error executing the query: ", err);
+      res.status(500).json({ error: "Failed to fetch user" });
+      return;
+    }
+    if (results.length === 0) {
+      res.status(404).json({ error: "User not found" });
+      return;
+    }
+    res.json(results[0]);
+  });
+});
+
+// POST /users - Create a new user
+app.post("/users", (req, res) => {
+  const { name, email } = req.body;
+  db.query(
+    "INSERT INTO users (name, email) VALUES (?, ?)",
+    [name, email],
+    (err, results) => {
+      if (err) {
+        console.error("Error executing the query: ", err);
+        res.status(500).json({ error: "Failed to create user" });
+        return;
+      }
+      res.json({
+        message: "User created successfully",
+        userId: results.insertId,
+      });
+    }
+  );
+});
+
+// PUT /users/:id - Update an existing user
+app.put("/users/:id", (req, res) => {
+  const userId = req.params.id;
+  const { name, email } = req.body;
+  db.query(
+    "UPDATE users SET name = ?, email = ? WHERE id = ?",
+    [name, email, userId],
+    (err, results) => {
+      if (err) {
+        console.error("Error executing the query: ", err);
+        res.status(500).json({ error: "Failed to update user" });
+        return;
+      }
+      if (results.affectedRows === 0) {
+        res.status(404).json({ error: "User not found" });
+        return;
+      }
+      res.json({ message: "User updated successfully" });
+    }
+  );
+});
+
+// DELETE /users/:id - Delete a user
+app.delete("/users/:id", (req, res) => {
+  const userId = req.params.id;
+  db.query("DELETE FROM users WHERE id = ?", [userId], (err, results) => {
+    if (err) {
+      console.error("Error executing the query: ", err);
+      res.status(500).json({ error: "Failed to delete user" });
+      return;
+    }
+    if (results.affectedRows === 0) {
+      res.status(404).json({ error: "User not found" });
+      return;
+    }
+    res.json({ message: "User deleted successfully" });
+  });
+});
+
+//---------------------------------TODOS-------------------------------------
 app.get("/todos", (req, res) => {
   db.query("SELECT * FROM todos", (err, results) => {
     if (err) {
@@ -25,6 +104,82 @@ app.get("/todos", (req, res) => {
   });
 });
 
+app.get("/todos/:id", (req, res) => {
+  const todoId = req.params.id;
+  db.query("SELECT * FROM todos WHERE id = ?", [todoId], (err, results) => {
+    if (err) {
+      console.error("Error executing the query: ", err);
+      res.status(500).json({ error: "Failed to fetch todo" });
+      return;
+    }
+    if (results.length === 0) {
+      res.status(404).json({ error: "Todo not found" });
+      return;
+    }
+    res.json(results[0]);
+  });
+});
+
+// POST /todos - Create a new todo
+app.post("/todos", (req, res) => {
+  const { title, completed, userId } = req.body;
+  db.query(
+    "INSERT INTO todos (title, completed, userId) VALUES (?, ?, ?)",
+    [title, completed, userId],
+    (err, results) => {
+      if (err) {
+        console.error("Error executing the query: ", err);
+        res.status(500).json({ error: "Failed to create todo" });
+        return;
+      }
+      res.json({
+        message: "Todo created successfully",
+        todoId: results.insertId,
+      });
+    }
+  );
+});
+
+// PUT /todos/:id - Update an existing todo
+app.put("/todos/:id", (req, res) => {
+  const todoId = req.params.id;
+  const { title, completed, userId } = req.body;
+  db.query(
+    "UPDATE todos SET title = ?, completed = ?, userId = ? WHERE id = ?",
+    [title, completed, userId, todoId],
+    (err, results) => {
+      if (err) {
+        console.error("Error executing the query: ", err);
+        res.status(500).json({ error: "Failed to update todo" });
+        return;
+      }
+      if (results.affectedRows === 0) {
+        res.status(404).json({ error: "Todo not found" });
+        return;
+      }
+      res.json({ message: "Todo updated successfully" });
+    }
+  );
+});
+
+// DELETE /todos/:id - Delete a todo
+app.delete("/todos/:id", (req, res) => {
+  const todoId = req.params.id;
+  db.query("DELETE FROM todos WHERE id = ?", [todoId], (err, results) => {
+    if (err) {
+      console.error("Error executing the query: ", err);
+      res.status(500).json({ error: "Failed to delete todo" });
+      return;
+    }
+    if (results.affectedRows === 0) {
+      res.status(404).json({ error: "Todo not found" });
+      return;
+    }
+    res.json({ message: "Todo deleted successfully" });
+  });
+});
+
+//------------------------------POSTS-------------------------------------------
 app.get("/posts", (req, res) => {
   db.query("SELECT * FROM posts", (err, results) => {
     if (err) {
@@ -36,6 +191,82 @@ app.get("/posts", (req, res) => {
   });
 });
 
+app.get("/posts/:id", (req, res) => {
+  const postId = req.params.id;
+  db.query("SELECT * FROM posts WHERE id = ?", [postId], (err, results) => {
+    if (err) {
+      console.error("Error executing the query: ", err);
+      res.status(500).json({ error: "Failed to fetch post" });
+      return;
+    }
+    if (results.length === 0) {
+      res.status(404).json({ error: "Post not found" });
+      return;
+    }
+    res.json(results[0]);
+  });
+});
+
+// POST /posts - Create a new post
+app.post("/posts", (req, res) => {
+  const { title, body, userId } = req.body;
+  db.query(
+    "INSERT INTO posts (title, body, userId) VALUES (?, ?, ?)",
+    [title, body, userId],
+    (err, results) => {
+      if (err) {
+        console.error("Error executing the query: ", err);
+        res.status(500).json({ error: "Failed to create post" });
+        return;
+      }
+      res.json({
+        message: "Post created successfully",
+        postId: results.insertId,
+      });
+    }
+  );
+});
+
+// PUT /posts/:id - Update an existing post
+app.put("/posts/:id", (req, res) => {
+  const postId = req.params.id;
+  const { title, body, userId } = req.body;
+  db.query(
+    "UPDATE posts SET title = ?, body = ?, userId = ? WHERE id = ?",
+    [title, body, userId, postId],
+    (err, results) => {
+      if (err) {
+        console.error("Error executing the query: ", err);
+        res.status(500).json({ error: "Failed to update post" });
+        return;
+      }
+      if (results.affectedRows === 0) {
+        res.status(404).json({ error: "Post not found" });
+        return;
+      }
+      res.json({ message: "Post updated successfully" });
+    }
+  );
+});
+
+// DELETE /posts/:id - Delete a post
+app.delete("/posts/:id", (req, res) => {
+  const postId = req.params.id;
+  db.query("DELETE FROM posts WHERE id = ?", [postId], (err, results) => {
+    if (err) {
+      console.error("Error executing the query: ", err);
+      res.status(500).json({ error: "Failed to delete post" });
+      return;
+    }
+    if (results.affectedRows === 0) {
+      res.status(404).json({ error: "Post not found" });
+      return;
+    }
+    res.json({ message: "Post deleted successfully" });
+  });
+});
+
+//----------------------COMMENTS------------------------------------
 app.get("/comments", (req, res) => {
   db.query("SELECT * FROM comments", (err, results) => {
     if (err) {
@@ -47,7 +278,87 @@ app.get("/comments", (req, res) => {
   });
 });
 
+app.get("/comments/:id", (req, res) => {
+  const commentId = req.params.id;
+  db.query(
+    "SELECT * FROM comments WHERE id = ?",
+    [commentId],
+    (err, results) => {
+      if (err) {
+        console.error("Error executing the query: ", err);
+        res.status(500).json({ error: "Failed to fetch comment" });
+        return;
+      }
+      if (results.length === 0) {
+        res.status(404).json({ error: "Comment not found" });
+        return;
+      }
+      res.json(results[0]);
+    }
+  );
+});
+
+// POST /comments - Create a new comment
+app.post("/comments", (req, res) => {
+  const { postId, name, email, body } = req.body;
+  db.query(
+    "INSERT INTO comments (postId, name, email, body) VALUES (?, ?, ?, ?)",
+    [postId, name, email, body],
+    (err, results) => {
+      if (err) {
+        console.error("Error executing the query: ", err);
+        res.status(500).json({ error: "Failed to create comment" });
+        return;
+      }
+      res.json({
+        message: "Comment created successfully",
+        commentId: results.insertId,
+      });
+    }
+  );
+});
+
+// PUT /comments/:id - Update an existing comment
+app.put("/comments/:id", (req, res) => {
+  const commentId = req.params.id;
+  const { postId, name, email, body } = req.body;
+  db.query(
+    "UPDATE comments SET postId = ?, name = ?, email = ?, body = ? WHERE id = ?",
+    [postId, name, email, body, commentId],
+    (err, results) => {
+      if (err) {
+        console.error("Error executing the query: ", err);
+        res.status(500).json({ error: "Failed to update comment" });
+        return;
+      }
+      if (results.affectedRows === 0) {
+        res.status(404).json({ error: "Comment not found" });
+        return;
+      }
+      res.json({ message: "Comment updated successfully" });
+    }
+  );
+});
+
+// DELETE /comments/:id - Delete a comment
+app.delete("/comments/:id", (req, res) => {
+  const commentId = req.params.id;
+  db.query("DELETE FROM comments WHERE id = ?", [commentId], (err, results) => {
+    if (err) {
+      console.error("Error executing the query: ", err);
+      res.status(500).json({ error: "Failed to delete comment" });
+      return;
+    }
+    if (results.affectedRows === 0) {
+      res.status(404).json({ error: "Comment not found" });
+      return;
+    }
+    res.json({ message: "Comment deleted successfully" });
+  });
+});
+
 // Start the server
-app.listen(3000, () => {
-  console.log("Server listening on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}...`);
 });

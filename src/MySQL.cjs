1651ef8cbc17709678bef17933cabc9bf8bef02b
Shirel <shkadosh@g.jct.ac.il>
2023-06-20 16:22:55 +0300

    server database

--- src/MySQL.cjs
+++ src/MySQL.cjs
@@ -1,23 +1,23 @@
-var mysql = require("mysql2");
+// var mysql = require("mysql2");
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "262652",
-  database: "jsonplaceholder",
-  port: "3306",
-});
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "262652",
+//   database: "jsonplaceholder",
+//   port: "3306",
+// });
 
-con.connect(function (err) {
-  if (err) {
-    return console.error("error: " + err.message);
-  }
-  console.log("Connected to MySQL server!");
-  con.query("select * from users", function (err, result) {
-    if (err) {
-      console.log(err.message);
-    }
-    console.log(result);
-  });
-  con.end();
-});
+// con.connect(function (err) {
+//   if (err) {
+//     return console.error("error: " + err.message);
+//   }
+//   console.log("Connected to MySQL server!");
+//   con.query("select * from users", function (err, result) {
+//     if (err) {
+//       console.log(err.message);
+//     }
+//     console.log(result);
+//   });
+//   con.end();
+// });

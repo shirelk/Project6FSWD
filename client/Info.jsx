4fd4ebb4eb8c67bbc772e24f695c0785d0222b26
rachel lea kohen <rkohen@g.jct.ac.il>
2023-06-22 14:43:35 +0300

    change some faild in info

--- client/Info.jsx
+++ client/Info.jsx
@@ -1,35 +1,43 @@
 import React, { useState } from "react";
 function Info() {
-  function getInfo(){
+  function getInfo() {
     let user = JSON.parse(localStorage.getItem("ourUser"));
-    return <>
-      <h2>{user.name}</h2>
-      <p>
-      username: {user.username} <br/>
-      email: {user.email}<br/>
-      address: <br/>
-        street: {user.address.street}<br/>
-        suite: {user.address.suite}<br/>
-        city: {user.address.city}<br/>
-        zipcode: {user.address.zipcode}<br/>
-        geo: <br/>
-          lat: {user.address.geo.lat}<br/>
-          lng: {user.address.geo.lng}<br/>
-          
-      phone: {user.phone}<br/>
-      website: {user.phone}<br/>
-      company: <br/>
-        name: {user.company.name}<br/>
-        catchPhrase: {user.company.catchPhrase}<br/>
-        bs: {user.company.bs}
-      </p>
-    </>
+    return (
+      <>
+        <h2>{user.name}</h2>
+        <p>
+          username: {user.username} <br />
+          email: {user.email}
+          <br />
+          address: <br />
+          street: {user.street}
+          <br />
+          suite: {user.suite}
+          <br />
+          city: {user.city}
+          <br />
+          zipcode: {user.zipcode}
+          <br />
+          geo: <br />
+          lat: {user.lat}
+          <br />
+          lng: {user.lng}
+          <br />
+          phone: {user.phone}
+          <br />
+          website: {user.phone}
+          <br />
+          company: <br />
+          name: {user.companyName}
+          <br />
+          catchPhrase: {user.catchPhrase}
+          <br />
+          bs: {user.bs}
+        </p>
+      </>
+    );
   }
-  return (
-    <>
-      {getInfo()}
-    </>
-  );
+  return <>{getInfo()}</>;
 }
 
 export default Info;

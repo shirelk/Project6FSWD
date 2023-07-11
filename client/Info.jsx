e12a23ca303646c10c942237d0c2d30d410f1b23
Shirel <shkadosh@g.jct.ac.il>
2023-07-11 18:03:53 +0300

    added btns to info

--- client/Info.jsx
+++ client/Info.jsx
@@ -49,16 +49,61 @@ function Info() {
           >
             change password
           </button>
-          <Popup trigger={buttonChangePassword} setTrigger={setButtonChangePassword}>
+          <Popup
+            trigger={buttonChangePassword}
+            setTrigger={setButtonChangePassword}
+          >
             <div className="popup-div">
-            {console.log("change password popup")}
-            <h3>change password</h3>
-            <h5>enter new password:</h5>
-            <input type="text" placeholder="new password"></input>
+              {console.log("change password popup")}
+              <h3>change password</h3>
+              <h5>enter new password:</h5>
+              <input type="text" placeholder="new password"></input>
             </div>
           </Popup>
-          <button className="crudBtn mainBtn">edit info</button>
-          <button className="crudBtn mainBtn">delete user</button>
+          <button
+            className="crudBtn mainBtn"
+            onClick={() => setButtonEditInfo(true)}
+          >
+            edit info
+          </button>
+          <Popup trigger={buttonEditInfo} setTrigger={setButtonEditInfo}>
+            <div className="popup-div">
+              {console.log("edit info popup")}
+              <h3>Edit info</h3>
+              {/* לעשות שיופיע בפלייסהולדר את הנתונים שיש עכשיו? */}
+              <input type="text" placeholder="Username"></input>
+              <input type="email" placeholder="Email"></input>
+              <input type="text" placeholder="Address"></input>
+              <input type="text" placeholder="Street"></input>
+              <input type="text" placeholder="Suit"></input>
+              <input type="text" placeholder="City"></input>
+              <input type="text" placeholder="Zip code"></input>
+              <input type="text" placeholder="Geo"></input>
+              <input type="text" placeholder="Latitude"></input>
+              <input type="text" placeholder="Longitude"></input>
+              <input type="text" placeholder="Phone"></input>
+              <input type="text" placeholder="Website"></input>
+              <input type="text" placeholder="Company"></input>
+              <input type="text" placeholder="Name"></input>
+              <input type="text" placeholder="catchPhrase"></input>
+              <input type="text" placeholder="bs"></input>
+            </div>
+          </Popup>
+          <button
+            className="crudBtn mainBtn"
+            onClick={() => setButtonDeleteUser(true)}
+          >
+            delete user
+          </button>
+          <Popup trigger={buttonDeleteUser} setTrigger={setButtonDeleteUser}>
+            <div className="popup-div">
+              {console.log("delete user popup")}
+              <h3>Are you sure you want to delete the user?</h3>
+              <h5>enter your password to confirm:</h5>
+              <input type="password" placeholder="password"></input>
+              {/* להוסיף בדיקה לסיסמה */}
+            </div>
+          </Popup>
         </div>
       </div>
     );
